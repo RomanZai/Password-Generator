530b0139f0c68dee8d7c3faf6ca67a2de23f961a
RomanZ <rom.zaikin@gmail.com>
2019-09-23 01:41:26 -0700

    Working code, no char limit, some design

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 document.body.style.background = "lightgray";
-document.getElementById('generate').setAttribute( "border-radius", "8px");
-document.getElementById('generate').setAttribute( "background-color", "red");
+
+
+document.getElementById('password').style.padding="65px";
+
+document.getElementById('generate').style.background="red";
+document.getElementById('generate').style.color="white";
+document.getElementById('generate').style.borderRadius="20px";
+document.getElementById('generate').style.padding="14px";
+
+document.getElementById('copy').style.background="grey";
+document.getElementById('copy').style.color="white";
+document.getElementById('copy').style.borderRadius="20px";
+document.getElementById('copy').style.padding="14px";
+
 
 var num = "0123456789";
 var low = "abcdefghijklmnopqrstuvwxyz";
@@ -13,15 +25,18 @@ var speBox = document.getElementById("spe");
 var lowBox = document.getElementById("low");
 var uppBox = document.getElementById("upp");
 
-generate.addEventListener("click",function(a){
-var characters = "";
-(numBox.checked) ? characters += num : '';
-(speBox.checked) ? characters += spe : '';
-(lowBox.checked) ? characters += low : '';
-(uppBox.checked) ? characters += upp : '';
-password.value = passwordGen(charNum.value, characters);
-});
 
+
+generate.addEventListener("click", generateNum);
+
+function generateNum() {
+    var characters = "";
+    (numBox.checked) ? characters += num : '';
+    (speBox.checked) ? characters += spe : '';
+    (lowBox.checked) ? characters += low : '';
+    (uppBox.checked) ? characters += upp : '';
+    password.value = passwordGen(charNum.value, characters); 
+}
     
     function passwordGen(length, characters){
         var gen = "";
@@ -31,3 +46,10 @@ password.value = passwordGen(charNum.value, characters);
 
     return gen;
     }
+
+    function myFunction() {
+        var copyText = document.getElementById("password");
+        copyText.select();
+        document.execCommand("copy");
+        alert("Copied the text: " + copyText.value);
+      }
